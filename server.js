8b1e05205636d89075fe585028480ea549051631
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-05-17 23:34:31 -0400

    Add registration process for new user

--- server.js
+++ server.js
@@ -43,7 +43,8 @@ myDB(async client => {
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
@@ -82,14 +83,45 @@ myDB(async client => {
 
   app.route('/profile')
     .get(ensureAuthenticated, (req, res) => {
-      res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
+      res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
     });
+
+  app.route('/register')
+    .post((req, res, next) => {
+      myDataBase.findOne({ username: req.body.username },  (err, user) => {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect('/');
+        } else {
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          },
+            (err, doc) => {
+              if (err) {
+                res.redirect('/');
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          )
+        }
+      })
+    },
+      passport.authenticate('local', { failureRedirect: '/' }),
+      (req, res, next) => {
+        res.redirect('/profile');
+      }
+    );
 
   app.route('/logout')
     .get((req, res) => {
       req.logout();
       res.redirect('/');
-  });
+    });
 
   app.use((req, res, next) => {
     res.status(404)
